9d1154d95be39169bdc3d2c01f4a22585ccb4bfe
jeffleon <jefferson.leon@usantotomas.edu.co>
2022-12-08 20:00:10 -0500

    fix error

--- src/config/app/config.ts
+++ src/config/app/config.ts
@@ -1,7 +1,7 @@
 export const ENV_CONFIG = {
     app: {
       port: process.env.PORT,
-      hostname: process.env.HOSTNAME, 
+      hostname: process.env.HOSTNAME,
       apiRoot: process.env.APIROOT,
     }
   };
